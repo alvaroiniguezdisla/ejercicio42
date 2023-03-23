bd3c4d44b9e322aac7e8ecb5e1cad4a963c422f9
Álvaro <ainiguezdislaa@gmail.com>
2023-03-23 16:23:21 +0100

    ej43

--- src/ejercicio43.java
+++ src/ejercicio43.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ejercicio43 {
+
+public class ejercicio43 {
+    public static boolean buscar(int e, int[] array) {
+        int i = array.length / 2;
+        while (i < array.length && i >= 0) {
+            if (array[i] == e) {
+                return true;
+            } else if (array[i] > e) {
+                i--;
+            } else if (array[i] < e) {
+                i++;
+            }
+
+
+        }
+        return false;
+    }
+
 }
